611e63375b8d06524577353623fe0911a8c1288d
Agnieszka Letkiewicz <a.letkiewicz@gmail.com>
2020-04-17 23:44:30 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,6 +4,8 @@ def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!" 
     return "I LOVE YOU TOO PUMPKIN!"
   elsif string.upcase == string
+    return "NO, NOT SINCE 1938!"
+  else
     return ""
   end
 end
